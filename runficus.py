a1b210a12342e2b0f866c2ee582585dddae2b021
databay <dennis.atabay@gmail.com>
2015-09-10 17:39:57 +0200

    add model

---
+++ runficus.py
@@ -0,0 +1,15 @@
+import ficus
+import os
+
+
+input_file = 'example.xlsx'
+
+result_folder = 'result'
+result_name = os.path.splitext(os.path.split(input_file)[1])[0]
+result_dir = ficus.prepare_result_directory(result_folder,result_name)
+
+prob = ficus.run_ficus(input_file, opt = 'mosek', neos=True)
+
+ficus.report(prob, result_dir)
+ficus.result_figures(result_dir,prob=prob, show=True)
+
